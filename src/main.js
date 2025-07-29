8a0fb459aa636d6f5fe1bce12f383b6263e90168
pook mini <mindhox@proton.me>
2025-07-29 16:27:31 +0200

    fix and com's added

--- src/main.js
+++ src/main.js
@@ -12,16 +12,18 @@ function randomPosition(min, max) {
 // reset de la carte P.
 reset_btn.addEventListener("click", () => {
   img_previews.innerHTML = "";
-  count.textContent = "0 élémen icits(s) sur la carte";
+  count.textContent = "0 éléments(s) sur la carte";
   textOnCard.style.zIndex = "10";
 });
 
 // ajout des stickers sur la carte P.
 sticker.forEach((sticker) => {
+  // pour tous les stickers j'ajoute un écouteur d'evenement
   sticker.addEventListener("click", () => {
+    // je cache le texte de la carte avec un z-index
     textOnCard.style.zIndex = "-10";
-
     const imgCard = document.createElement("img");
+    // ajoute le style au sticker
     imgCard.classList.add("img_card");
     imgCard.style.backgroundColor = "transparent";
     imgCard.style.border = "none";
@@ -38,13 +40,19 @@ sticker.forEach((sticker) => {
       0,
       img_previews.offsetWidth - 50
     )}px`;
+    // j'ajoute le sticker sur la carte avec sa source et son alt
     imgCard.src = sticker.src;
     imgCard.alt = sticker.alt;
+
+    // j'ajoute le sticker sur la carte en utilisant appenchild()
     img_previews.appendChild(imgCard);
+
+    // je concatenne le nombre de stickers sur la carte
     count.textContent =
       img_previews.querySelectorAll(".img_card").length +
       " éléments(s) sur la carte";
 
+    // je créer une boucle for afin d'ajouter un ecouteur d'evenement afin de supprimer les stickers
     for (const img_preview of document.querySelectorAll(".img_card")) {
       img_preview.addEventListener("click", () => {
         img_preview.remove();
